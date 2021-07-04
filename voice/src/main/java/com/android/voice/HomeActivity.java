d9a4dc66c1fe8d0f922180d8fa42190f67914027
wgueter <mwminhhui@gmail.com>
2021-07-05 00:35:15 +0800

    首页框架搭建

--- voice/src/main/java/com/android/voice/HomeActivity.java
+++ voice/src/main/java/com/android/voice/HomeActivity.java
@@ -1,10 +1,26 @@
 package com.android.voice;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
-public class HomeActivity extends FragmentActivity {
+import com.android.voice.model.CHANNEL;
+
+public class HomeActivity extends FragmentActivity implements View.OnClickListener{
+
+    /**
+     * 指定首页出现的卡片
+     */
+    private static final CHANNEL[] CHANNELS =
+            new CHANNEL[]{CHANNEL.MY, CHANNEL.DISCORY, CHANNEL.FRIEND};
+
+    private DrawerLayout mDrawerLayout;
+    private View mToggleView;
+    private View mSearchView;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +30,26 @@ public class HomeActivity extends FragmentActivity {
         initData();
     }
 
+    private void intiView() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggleView = findViewById(R.id.toggle_view);
+        mToggleView.setOnClickListener(this);
+        mSearchView = findViewById(R.id.search_view);
+
+        mViewPager = findViewById(R.id.view_pager);
+        initMagicIndicator();
+    }
+
+    private void initMagicIndicator() {
+
+    }
+
     private void initData() {
     }
 
-    private void intiView() {
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
